eab6aa76c31230ea06fce5512e2b4e6a8e87a151
sujal patel <worldonwheels701@gmail.com>
2024-08-03 23:45:46 -0500

    first commit

---
+++ src/dbConfig/dbconfig.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try{
+        mongoose.connect(process.env.MONGODB_URI!)
+        const connection = mongoose.connection;
+
+        connection.on("connected", () => {
+           console.log("connected to database")
+        })
+
+        connection.on("error", (err) => {
+            console.log("error connecting to database")
+            console.log(err)
+        })
+    }
+    catch(err){
+        console.log("something went wrong")
+        console.log(err)
+    }
+}
